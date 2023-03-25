d6d5eef6cbdf44ae5bbad1caf085714c8f738730
unknown <sawadbannur@gmail.com>
2023-03-25 15:13:11 +0530

    inserting value into user record

--- server/sqlscripts.py
+++ server/sqlscripts.py
@@ -7,8 +7,10 @@ import sqlite3
 # conn = sqlite3.connect('budgetify.db')
 conn = sqlite3.connect('budgetify.db')
 
+
 def create_user_records_table():
-    conn.execute('''CREATE TABLE  IF NOT EXISTS USER_RECORD
+     c=conn.cursor()
+     c.execute('''CREATE TABLE  IF NOT EXISTS USER_RECORD
          (User_ID INTEGER PRIMARY KEY AUTOINCREMENT,
          First_name VARCHAR(50),
          Last_name  VARCHAR(50),
@@ -18,4 +20,36 @@ def create_user_records_table():
          DOB        VARCHAR(10),
          Job_title  VARCHAR(30),
          City       VARCHAR(35));''')
-    print("Table created successfully")
+     print("Table created successfully")
+     conn.commit()
+
+     
+
+def insert_value(firstname, lastname, email, password, gender,dob, jobtitle, city):
+     c=conn.cursor()
+     c.execute("INSERT INTO USER_RECORD (First_name,Last_name,Email,Password,Gender,DOB,Job_title,City) VALUES ('?','?','?','?','?','?','?','?')",firstname, lastname, email, password, gender,dob, jobtitle, city)
+     print("inserted succefully")
+     conn.commit()
+create_user_records_table()
+insert_value()
+
+
+
+# many=[  ('shah','rukh','shahrukh@gmail.com','sawad2051','M','03-11-1956','king','bombay')]
+
+# def insert_many(many):
+#      conn = sqlite3.connect('budgetify.db')
+#      c=conn.cursor()
+#      c.executemany("INSERT INTO budgetify VALUES (?,?,?,?,?,?,?,?)",many)
+#      print("inserted succesufuuly")
+#      conn.commit()
+#      conn.close()
+
+
+
+
+
+
+
+
+conn.close()

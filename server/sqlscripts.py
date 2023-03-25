5345e731c4d2228a2df37f5358b7bf3540b69c3a
unknown <muhammedafham22@gmail.com>
2023-03-25 11:47:12 +0530

    database created and user record tables created

--- server/sqlscripts.py
+++ server/sqlscripts.py
@@ -1,2 +1,22 @@
-def hello():
-    print('hello from sql script')
+import sqlite3
+
+#--To create databse--
+# conn = sqlite3.connect('budgetify.db')
+# print('Database created')
+
+# conn = sqlite3.connect('budgetify.db')
+
+def create_user_records_table():
+    conn = sqlite3.connect('budgetify.db')
+    conn.execute('''CREATE TABLE  IF NOT EXISTS USER_RECORD
+         (User_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+         First_name VARCHAR(50),
+         Last_name  VARCHAR(50),
+         Email      VARCHAR(50),
+         Password   VARCHAR(50),
+         Gender     CHAR(1),
+         DOB        VARCHAR(10),
+         Job_title  VARCHAR(30),
+         City       VARCHAR(35));''')
+    print("Table created successfully")
+    conn.close()

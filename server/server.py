e6f3afa211cca966297de15bd9d61c7b3444a750
unknown <muhammedafham22@gmail.com>
2023-04-09 13:41:53 +0530

    sync

--- server/server.py
+++ server/server.py
@@ -1,15 +1,45 @@
-import sqlscripts as sqls,computations as comp
+import sqlscripts as sqls,tokenjwt as jt
 # Using flask to make an api
 # import necessary libraries and functions
 from flask import Flask, jsonify, request, make_response
 #import config
 #import jwt
-
+import jwt
+from decouple import config
 # creating a Flask app
 app = Flask(__name__)
 
 #--Creating userrecord table--
 # sqls.create_user_records_table()
+def auth_required(endpoint_name):
+    def decorated(view_func):
+        def wrapper(*args, **kwargs):
+            auth_header = request.headers.get('Authorization')
+            if not auth_header:
+                return jsonify({'message': 'Authorization header is missing'}), 401
+            # Extract the token from the header
+            token = auth_header.split(' ')[1]
+            try:
+                # Verify the token
+                payload = jwt.decode(token, jt.public_key, algorithms=['RS256'])
+            except jwt.InvalidTokenError:
+                return jsonify({'message': 'Invalid token'}), 401
+
+            # Add the decoded token to the request context
+            request.decoded_token = payload
+            return view_func(*args, **kwargs)
+
+        wrapper.__name__ = view_func.__name__
+        wrapper.__doc__ = view_func.__doc__
+        wrapper.__module__ = view_func.__module__
+
+        app.add_url_rule('/' + endpoint_name, view_func=wrapper)
+        return wrapper
+
+    return decorated
+
+
+
 
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
@@ -25,9 +55,11 @@ def createaccount():
 	dob = data['dob']
 	jobtitle = data['jobtitle']
 	city = data['city']
-	sqls.insert_value(firstname, lastname, email, password, gender,dob, jobtitle, city)
-	sqls.create_user_expense_table(email)
-	return jsonify({'result':'success', 'token':'1234'})
+	user_id,lastname=sqls.createAccount(firstname, lastname, email, password, gender,dob, jobtitle, city)
+	payload = {'user_id':user_id,'lastname':lastname}
+	token=jwt.encode(payload,jt.private_key, algorithm='RS256')
+	sqls.create_user_expense_table(user_id,lastname)
+	return jsonify({'token':token})
 
 
 @app.route('/login', methods = ['POST'])
@@ -42,41 +74,43 @@ def login():
 	# 	return 'Invalid password', 401
 
 @app.route('/PPPCalculation', methods = ['GET'])
+@auth_required('PPPCalculation')
 def PPPCalc():
 	return 'OOK'
 
+# @app.route('/DemoCompare', methods = ['GET'])
+# @auth_required('DemoCompare_auth')
+# def demoCompare():
+# 	data=request.get_json()
+# 	age=data['age']
+# 	income=data['income']
+# 	job_title=data['job_title']
+# 	gender=data['gender']
+# 	city=data['city']
 
-@app.route('/DemoCompare', methods = ['GET'])
-def demoCompare():
-	data=request.get_json()
-	age=data['age']
-	income=data['income']
-	job_title=data['job_title']
-	gender=data['gender']
-	city=data['city']
-
-	pred=comp.evaluate(age,income,job_title,gender,city)
-	return jsonify({'Housing':str (pred[0]),
-		 'Groceries':str (pred[1]),
-		 'Entertainment':str (pred[2]),
-		 'Leisure':str (pred[3]),
-		 'Transportation':str (pred[4]),
-		 'Medical':str (pred[5]),
-		 'Utilities':str (pred[6]),
-		 'Insurance':str (pred[7])
-		 })
+# 	pred=comp.evaluate(age,income,job_title,gender,city)
+# 	return jsonify({'Housing':str (pred[0]),
+# 		 'Groceries':str (pred[1]),
+# 		 'Entertainment':str (pred[2]),
+# 		 'Leisure':str (pred[3]),
+# 		 'Transportation':str (pred[4]),
+# 		 'Medical':str (pred[5]),
+# 		 'Utilities':str (pred[6]),
+# 		 'Insurance':str (pred[7])
+# 		 })
 	
 
 
-
 @app.route('/insertTransac', methods = ['POST'])
+@auth_required('insertTransac')
 def insertTransac():
-	data=request.get_json()
-	tid=data['tid']
-	date=data['date']
-	category=data['category']
-	amount=data['amount']
-	sqls.insert_into_transac(tid,date,category,amount)
+	# user_id = request.decoded_token.get('user_id')
+	# data=request.get_json()
+	# tid=data['tid']
+	# date=data['date']
+	# category=data['category']
+	# amount=data['amount']
+	# sqls.insert_into_transac(user_id,tid,date,category,amount)
 	return 'Transaction inserted successfully'
 
 
@@ -99,11 +133,18 @@ def checkEmail():
 # 	return jsonify({'data': num**2})
 
 
-# def jwtTokenizer():
-# 	return jwt.encode({'some': 'payload'},config('SECRET_KEY'), algorithm='RS256')
 
+@app.route('/token', methods = ['POST'])
+@auth_required('token_auth')
+def token():
+	user_id = request.decoded_token.get('user_id')
+	lastname = request.decoded_token.get('lastname')
 
-
+	# a=jwt.encode({'user_id': '1234'},jt.private_key, algorithm='RS256')
+	# print(a)
+	# b=jwt.decode(a,jt.public_key, algorithms=["RS256"])
+	# print(b)
+	return jsonify({'id':user_id,'lastname':lastname})
 # driver function
 if __name__ == '__main__':
 
--- server/tokenjwt.py
+++ server/tokenjwt.py
@@ -1,6 +1,4 @@
-import jwt
-from decouple import config
-private_key = b'''-----BEGIN RSA PRIVATE KEY-----
+private_key = '''-----BEGIN RSA PRIVATE KEY-----
 MIICWwIBAAKBgGG+Fr74dguj377DmNH5zrFBbayspsb1Vu/5Vo63ljRyemzuzMys
 FV0MACdN3xiBm96RMjIVdnHf2XEUvQkhWKUB5CI0pjtXOGtwwofRE6660cONb4Ye
 SbY54x6+PXY2dy1JXLpVkuJRMDRceFvFG4FDYi7cTLepQO2M9+4HOWKDAgMBAAEC
@@ -15,13 +13,33 @@ Sg6OQDCZFL+VAvucZlKjzZ19OfvL5PxWR4AcLJF2PlKtp69qDeVSKaSBOAmN4iMs
 6X7q+mMBaeG5v8m5AkEApE5n6m6bYMWqfxTH+2ZdZST5v6/p10C+kXmBtWXzotTw
 nejI+0O4tQ2o0mGodRFXRFTbNrssbjixTO8kMb+3VA==
 -----END RSA PRIVATE KEY-----'''
-public_key = b'''-----BEGIN PUBLIC KEY-----
+public_key = '''-----BEGIN PUBLIC KEY-----
 MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGG+Fr74dguj377DmNH5zrFBbays
 psb1Vu/5Vo63ljRyemzuzMysFV0MACdN3xiBm96RMjIVdnHf2XEUvQkhWKUB5CI0
 pjtXOGtwwofRE6660cONb4YeSbY54x6+PXY2dy1JXLpVkuJRMDRceFvFG4FDYi7c
 TLepQO2M9+4HOWKDAgMBAAE=
 -----END PUBLIC KEY-----'''
-a=jwt.encode({'user_id': '1234'},private_key, algorithm='RS256')
-print(a)
-b=decoded = jwt.decode(a, public_key, algorithms=["RS256"])
-print(b)
+
+# private_key = config('PRIVATE_KEY')
+# print(private_key)
+# # public_key = config('public_key')
+# # # print(public_key)
+# a=jwt.encode({'user_id': '1234'},private_key, algorithm='RS256')
+# # print(a)
+# # b=decoded = jwt.decode(a, public_key, algorithms=["RS256"])
+# # print(b)
+
+# from decouple import config
+# import jwt
+
+# private_key = config('PRIVATE_KEY')
+# public_key = config('PUBLIC_KEY')
+
+# payload = {'data': 'example'}
+# algorithm = 'RS256'
+# # create JWT
+# # jwt_token = jwt.encode(payload, private_key, algorithm=algorithm)
+
+# # # verify JWT
+# # decoded = jwt.decode(jwt_token, public_key, algorithms=[algorithm])
+# # print(decoded)

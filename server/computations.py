edd494861e039f9f785fa3671433b05d462e7270
Uvais-1 <4so19cs092.uvais@sjec.ac.in>
2023-04-15 01:23:30 +0530

    Implemented PPP

--- server/computations.py
+++ server/computations.py
@@ -25,7 +25,7 @@ print('done')
 # Evaluating the model on the testing data
     # score = model.score(X_test, y_test)
     # print('R^2 score:', score)
-    
+
 
 
 def evaluate(age,income,job_title,gender,city):
@@ -47,5 +47,38 @@ def evaluate(age,income,job_title,gender,city):
     print(predictions)
 # print predicted values
     return predictions[0]
-    
+
 # train()
+
+
+def PPP():
+    # Load the dataset into a Pandas DataFrame
+    df = pd.read_csv('dataset.csv')
+
+    city2 = 'Bangalore'
+    city3 = input('Enter city: ')
+
+    category_list = [('Housing', 17371), ('Entertainment', 5000)]
+
+    # Calculate the average expenses for each category for the two cities
+    result = df.groupby(['City'])[['Housing', 'Leisure', 'Entertainment', 'Insurance', 'Medical','Transportation','Groceries','Utilities']].mean().loc[[city2]]
+    result_dest = df.groupby(['City'])[['Housing', 'Leisure', 'Entertainment', 'Insurance', 'Medical','Transportation','Groceries','Utilities']].mean().loc[[city3]]
+
+    # Calculate the ratio for each category based on the provided list
+    ratio_list = [(category, value / result.loc[city2][category]) for category, value in category_list]
+
+    # Calculate the expenses for each category in the destination city based on the ratio list
+    expenses_city = pd.DataFrame([value * result_dest.loc[city3][category] for category, value in ratio_list], index=[category for category, value in ratio_list], columns=['Expenses'])
+
+    # Print the results
+    print(f'\nAverage expenses for {city2}: \n{result.loc[city2]}')
+    print(f'\nAverage expenses for {city3}: \n{result_dest.loc[city3]}')
+    print(f'\nExpenses for {city3} based on the provided category list: \n{expenses_city}')
+    # Convert expenses_city to a dictionary
+    expenses_dict = expenses_city.to_dict()
+
+    # Convert the dictionary to a JSON string
+    expenses_json = json.dumps(expenses_dict)
+
+    # Print the JSON string
+    print(expenses_json)
